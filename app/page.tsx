0f83bd7d39643f19f2dec4e1966f5e1de9acef8f
luciferdk <dksinha0701@gmail.com>
2025-03-29 15:21:46 +0530

    remote extra component from homePage coz nextjs not alloud

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,13 @@ import NavbarLayout from "../component/navlayout/navlayout";
 
 export default function Home() {
   return (
-    <div className="   ">
       <NavbarLayout>
       <p>{`"Hello"`}</p>
       <p>{`"World"`}</p>
       
-      <HomePage />
       </NavbarLayout>
-    </div>
+    
   );
 }
 
 
-export function HomePage () {
-  return (
-    <div>
-     <p>{`"Hello Home Page"`}</p>
-
-    
-    </div>
-  )   
-}
